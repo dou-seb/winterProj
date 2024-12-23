b4af73d45af4266a8e5cbdb7bc2c859988991c9b
douses <douses@coventry.ac.uk>
2024-12-23 14:28:42 +0000

    Image to tensor conversion

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+from PIL import Image
+
+img_path = r'C:\Users\Sebastien Douse\Documents\GitHub\Winter-Mini-Project\cats-in-the-wild-image-classification\versions\1\train\AFRICAN LEOPARD\001.jpg'
+image = Image.open(img_path)
+transform = transforms.ToTensor()
+ImgTensor = transform(image)
+print(ImgTensor.shape)
